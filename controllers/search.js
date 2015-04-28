a934871b41e7f7f494e185ddf57122c1e11f281e
yixuaning <yixuaning@sina.com>
2015-04-28 19:14:06 +0800

    设置百度为站内搜索引擎

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,7 @@
 exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
-  res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:cnodejs.org+' + q);
+  //res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:cnodejs.org+' + q);
+  //百度站内搜索
+  res.redirect('http://www.baidu.com/s?si=37iot.com&cl=3&ct=2097152&tn=baidulocal&word=' + q);
 };
